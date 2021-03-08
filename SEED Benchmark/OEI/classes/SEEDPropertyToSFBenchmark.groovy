239da3e6bb181b7bce8da2cc24c5a3c3adb2566c
Ted Summer <ted.summer2@gmail.com>
2021-03-08 12:07:06 -0700

    refactor: add Jeff's updates

--- SEED Benchmark/OEI/classes/SEEDPropertyToSFBenchmark.groovy
+++ SEED Benchmark/OEI/classes/SEEDPropertyToSFBenchmark.groovy
@@ -11,6 +11,7 @@ Benchmark = [
 
 	// Custom fields per Org
 	oei__Contact_Name__c: flowVars["ContactResults"],
+	oei_Property_Data_Administrator__c: flowVars["AdminContactResults"],
 	oei__Diff_from_National_Median_Site_EUI__c: propData["% Difference from National Median Site EUI"],
 	oei__Diff_than_National_Median_Source_EUI__c: propData["% Difference from National Median Source EUI"],
 	oei__ENERGY_STAR_Score__c: propData["ENERGY STAR Score"],
