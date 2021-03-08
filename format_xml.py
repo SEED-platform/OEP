b9f74f1de0a2bc34d979404adc0baddd1bc2f7d9
Ted Summer <ted.summer2@gmail.com>
2021-03-08 11:50:28 -0700

    fix: keep CDATA in xml formatter

--- format_xml.py
+++ format_xml.py
@@ -2,7 +2,11 @@ import sys
 
 import lxml.etree as etree
 
+parser = etree.XMLParser(strip_cdata=False)
 filename = sys.argv[1]
-tree = etree.parse(filename)
+
+with open(filename) as f:
+    tree = etree.XML(f.read().encode(), parser=parser)
+
 with open(filename, 'wb') as f:
     f.write(etree.tostring(tree, pretty_print=True))
